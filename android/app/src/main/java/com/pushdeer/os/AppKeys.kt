d504a91dc48ffdb80a42a5f6ddc920229339f3ed
Easy <easychen@qq.com>
2022-06-11 18:21:05 +0800

    Create AppKeys.kt

---
+++ android/app/src/main/java/com/pushdeer/os/AppKeys.kt
@@ -0,0 +1,9 @@
+package com.pushdeer.os.values
+
+object AppKeys {
+
+    const val MiPush_Id = "2882303761520124028"
+    const val MiPush_Key = "..."
+
+    const val WX_Id = "wx3ae07931d0555a24"
+}
