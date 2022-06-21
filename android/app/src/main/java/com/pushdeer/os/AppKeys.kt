a218d5c68d64b652c4d73739f2e6315b9c301d0f
Easy <easychen@qq.com>
2022-06-21 22:29:33 +0800

    add mipush key

--- android/app/src/main/java/com/pushdeer/os/AppKeys.kt
+++ android/app/src/main/java/com/pushdeer/os/AppKeys.kt
@@ -3,7 +3,7 @@ package com.pushdeer.os.values
 object AppKeys {
 
     const val MiPush_Id = "2882303761520124028"
-    const val MiPush_Key = "..."
+    const val MiPush_Key = "5512012428028"
 
     const val WX_Id = "wx3ae07931d0555a24"
 }
